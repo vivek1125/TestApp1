1c09b1fc4564f830ceb58b7a07d9f5163042b1d2
Raj <vivek.d.raj@accenture.com>
2020-07-02 13:54:24 +0530

    Add .cs file from local to github

---
+++ TestAppCsss.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestApp1
+{
+    class TestAppCsss
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // commit 1 : add a new line
+            //new
+            // commit 2: after add to azure
+            // commit 3: ok
+            Console.WriteLine("Hello World !");
+        }
+    }
+}
